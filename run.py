8b61b32269eddc4725b3d9387165908fce824e7b
Julmice Mardoche <julmicemardoche@gmail.com>
2025-10-30 13:59:02 -0400

    Initial commit with basic Flask setup

--- run.py
+++ run.py
@@ -13,4 +13,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # Démarrer le serveur de développement
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
